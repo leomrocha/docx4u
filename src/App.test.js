e300656809a8497e0831e3b3d495e637fc3666da
Leo Rocha <leo.m.rocha@gmail.com>
2020-12-05 17:56:54 +0100

    [form extraction][s] Starting to extract the elements from the word document to automatically create the form

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("App exists", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/Home/i);
+  // TODO this is some test that
+  expect(true).toBeTruthy()
 });
--- src/utils/template2form.js
+++ src/utils/template2form.js
@@ -3,24 +3,81 @@
  *
  */
 import fs from "fs";
-import { TemplateHandler } from 'easy-template-x';
+import { TemplateHandler, ScopeData } from 'easy-template-x';
 
+
+class CustomTemplateHandler extends (TemplateHandler) {
+  async getFields(templateFile, data) {
+    var xmlFields = []
+
+        // load the docx file
+        const docx = await this.loadDocx(templateFile);
+
+        // prepare context
+        const scopeData = new ScopeData(data);
+        const context = {
+            docx,
+            currentPart: null
+        };
+
+    const contentParts = await docx.getContentParts()
+    
+        for (const part of contentParts) {
+
+            context.currentPart = part;
+
+            // extensions - before compilation
+            await this.callExtensions(this.options.extensions?.beforeCompilation, scopeData, context);
+
+            // compilation (do replacements)
+            const xmlRoot = await part.xmlRoot();
+            xmlFields.push(xmlRoot)
+        }
+
+        // export the result
+        return xmlFields
+
+    }
+
+}
 
 /**
  *
  * @param {*} fname file name and path from where to extract the template fields for the form
  * @returns (template, fields) template object (to fill it later) and the fields (with proper elements) for the FORM
  */
-function extract_fields(fname) {
-  const templateFile = fs.readFileSync(fname);
-  console.log(templateFile)
+async function extractFields(fname) {
+
+  // unless something else happens, I'll set the tags 
+  const handler = new CustomTemplateHandler({
+              delimiters: {
+                  tagStart: '{%',
+                  tagEnd: '%}',
+                  containerTagOpen: '{>',
+                  containerTagClose: '<}'
+              }
+          });
+  const templateFile = fs.readFileSync(fname)
+  // console.log(templateFile)
+
+  try {
+    const fields = await handler.getFields(templateFile)
+    return fields
+  } catch (e) {
+    console.log("ERROR reading the file")
+    console.error(e)
+  }
+  
+  // return (templateFile, fields)
 }
 
-function fill_templates() {
-  // TODO
+async function fillTemplate(handler, template, data) {
+  const doc = await handler.process(template, data);
+  const docXml = await handler.getXml(doc);
+  return docXml
 }
 
 export {
-    extract_fields,
-    fill_templates
+    extractFields,
+    fillTemplate
 }
