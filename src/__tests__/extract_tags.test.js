948a06649d9ce1d3c9bd9de68d68cccd69f89ed7
Leo Rocha <leo.m.rocha@gmail.com>
2020-12-05 23:06:41 +0100

    [delimiters][s] Delimiters changed to a more unique one. Manual testing seems to work but the groups are not done correctly, needs some work there, check if it is on the plugins or on the TemplateHandler

---
+++ src/__tests__/extract_tags.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+// const request = require('supertest')
+var assert = require("assert");
+const fs = require("fs");
+
+var { extractTags, fillTemplate } = require("../utils/template2form");
+
+test("tries to open a template file", async () => {
+  const obj = await extractTags(
+    // "src/__tests__/fixtures/files/empty tag.docx"
+    // "src/__tests__/fixtures/files/header and footer.docx"
+    "src/__tests__/fixtures/files/header and footer - loop.docx"
+    // "src/__tests__/fixtures/files/facture.docx"
+    // "src/__tests__/fixtures/files/real life - he.docx"
+    // "src/__tests__/fixtures/files/custom delimiters.docx"
+    // "src/__tests__/fixtures/files/simple.docx"
+    // "src/__tests__/fixtures/files/link.docx"
+  );
+  console.log("Fields = ", obj);
+  assert(true);
+});
--- src/utils/template2form.js
+++ src/utils/template2form.js
@@ -3,42 +3,44 @@
  *
  */
 import fs from "fs";
-import { TemplateHandler, ScopeData } from 'easy-template-x';
+import { TemplateHandler, ScopeData } from "easy-template-x";
 
-
-class CustomTemplateHandler extends (TemplateHandler) {
+class CustomTemplateHandler extends TemplateHandler {
   async getFields(templateFile, data) {
-    var xmlFields = []
+    var xmlFields = [];
 
-        // load the docx file
-        const docx = await this.loadDocx(templateFile);
+    // load the docx file
+    const docx = await this.loadDocx(templateFile);
 
-        // prepare context
-        const scopeData = new ScopeData(data);
-        const context = {
-            docx,
-            currentPart: null
-        };
+    // prepare context
+    const scopeData = new ScopeData(data);
+    const context = {
+      docx,
+      currentPart: null,
+    };
 
-    const contentParts = await docx.getContentParts()
-    
-        for (const part of contentParts) {
+    const contentParts = await docx.getContentParts();
 
-            context.currentPart = part;
+    for (const part of contentParts) {
+      context.currentPart = part;
 
-            // extensions - before compilation
-            await this.callExtensions(this.options.extensions?.beforeCompilation, scopeData, context);
+      const xmlRoot = await part.xmlRoot();
+      const tags = this.compiler.parseTags(xmlRoot);
+      console.log("tags Type ", typeof tags);
+      console.log("Tags found: ", tags);
+      // console.log("Found Node: ", xmlRoot.childNodes)
+      // console.log("Node Keys: ", Object.keys(xmlRoot.childNodes))
+      // for (const obj in xmlRoot.childNodes) {
+      //   console.log("part text: ", JSON.stringify(obj))
+      // }
 
-            // compilation (do replacements)
-            const xmlRoot = await part.xmlRoot();
-            xmlFields.push(xmlRoot)
-        }
-
-        // export the result
-        return xmlFields
-
+      if (tags.length > 0) xmlFields.push(tags);
     }
 
+    // export the result
+
+    return xmlFields;
+  }
 }
 
 /**
@@ -46,38 +48,50 @@ class CustomTemplateHandler extends (TemplateHandler) {
  * @param {*} fname file name and path from where to extract the template fields for the form
  * @returns (template, fields) template object (to fill it later) and the fields (with proper elements) for the FORM
  */
-async function extractFields(fname) {
-
-  // unless something else happens, I'll set the tags 
+async function extractTags(fname) {
+  // unless something else happens, I'll set the tags
   const handler = new CustomTemplateHandler({
-              delimiters: {
-                  tagStart: '{%',
-                  tagEnd: '%}',
-                  containerTagOpen: '{>',
-                  containerTagClose: '<}'
-              }
-          });
-  const templateFile = fs.readFileSync(fname)
+    //TODO make the delimiters custom
+    delimiters: {
+      tagStart: "{%",
+      tagEnd: "%}",
+      // tagStart: "{{",
+      // tagEnd: "}}",
+      containerTagOpen: ">>",
+      containerTagClose: "<<",
+    },
+  });
+  const templateFile = fs.readFileSync(fname);
   // console.log(templateFile)
 
   try {
-    const fields = await handler.getFields(templateFile)
-    return fields
+    const fields = await handler.getFields(templateFile);
+    // console.log("doc text  = ", await handler.getText(templateFile));
+    // console.log("Received fields = ", fields);
+    return fields;
   } catch (e) {
-    console.log("ERROR reading the file")
-    console.error(e)
+    console.log("ERROR reading the file");
+    console.error(e);
   }
-  
+
   // return (templateFile, fields)
+}
+
+/**
+ * Converts a list of tags into a json
+ * @param {*} tagList A list of lists As returned by extractTags in this file
+ */
+function tags2json(tagList) {
+  // TODO
+  // each list tagList can contain different groups of tags, I'll start by the simplest ones
+  for (const tag of tagList) {
+  }
 }
 
 async function fillTemplate(handler, template, data) {
   const doc = await handler.process(template, data);
   const docXml = await handler.getXml(doc);
-  return docXml
+  return docXml;
 }
 
-export {
-    extractFields,
-    fillTemplate
-}
+export { extractTags, fillTemplate };
