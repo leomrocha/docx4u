e8e92a016ed5ab553440eb06e906421e0ad32a85
Leo Rocha <leo.m.rocha@gmail.com>
2020-12-06 18:26:08 +0100

    [form][s] FORM fields extraction from the template

--- src/__tests__/extract_tags.test.js
+++ src/__tests__/extract_tags.test.js
@@ -3,7 +3,11 @@ import React from "react";
 var assert = require("assert");
 const fs = require("fs");
 
-var { extractTags, fillTemplate } = require("../utils/template2form");
+var {
+  extractTags,
+  fillTemplate,
+  tags2json,
+} = require("../utils/template2form");
 
 test("tries to open a template file", async () => {
   const obj = await extractTags(
@@ -17,6 +21,29 @@ test("tries to open a template file", async () => {
     // "src/__tests__/fixtures/files/simple.docx"
     // "src/__tests__/fixtures/files/link.docx"
   );
-  console.log("Fields = ", obj);
+
+  console.log("Object Fields = ", obj);
+  // console.log("xml Object Fields = ", obj["obj"]);
+  // console.log("JSON fields = ", JSON.stringify(obj["json"], null, 2));
+  // console.log("FORM fields = ", JSON.stringify(obj["form"], null, 2));
+  console.log("FORM fields = ", obj["form"]);
+  // TODO
   assert(true);
 });
+
+// test("opens a template file and converts it to json", async () => {
+//   const obj = await extractTags(
+//     // "src/__tests__/fixtures/files/empty tag.docx"
+//     // "src/__tests__/fixtures/files/header and footer.docx"
+//     // "src/__tests__/fixtures/files/header and footer - loop.docx"
+//     // "src/__tests__/fixtures/files/loop - table.docx"
+//     "src/__tests__/fixtures/files/facture.docx"
+//     // "src/__tests__/fixtures/files/real life - he.docx"
+//     // "src/__tests__/fixtures/files/custom delimiters.docx"
+//     // "src/__tests__/fixtures/files/simple.docx"
+//     // "src/__tests__/fixtures/files/link.docx"
+//   );
+//   const jsn = tags2json(obj);
+//   console.log("JSON  = ", jsn);
+//   assert(true);
+// });
--- src/utils/template2form.js
+++ src/utils/template2form.js
@@ -4,11 +4,23 @@
  */
 import fs from "fs";
 import { TemplateHandler, ScopeData } from "easy-template-x";
+import { group } from "console";
+
+function singleTag2Json(tag) {
+  const jsn = {
+    rawText: String(tag.rawText),
+    disposition: String(tag.disposition),
+    name: String(tag.name),
+  };
+  return jsn;
+  // return JSON.parse(JSON.stringify(jsn));
+}
 
 class CustomTemplateHandler extends TemplateHandler {
   async getFields(templateFile, data) {
     var xmlFields = [];
-
+    var jsonFields = [];
+    var jsonForm = [];
     // load the docx file
     const docx = await this.loadDocx(templateFile);
 
@@ -26,19 +38,71 @@ class CustomTemplateHandler extends TemplateHandler {
 
       const xmlRoot = await part.xmlRoot();
       const tags = this.compiler.parseTags(xmlRoot);
-      console.log("tags Type ", typeof tags);
-      console.log("Tags found: ", tags);
+      // console.log("tags Type ", typeof tags);
+      // console.log("Tags found: ", tags);
       // console.log("Found Node: ", xmlRoot.childNodes)
       // console.log("Node Keys: ", Object.keys(xmlRoot.childNodes))
       // for (const obj in xmlRoot.childNodes) {
       //   console.log("part text: ", JSON.stringify(obj))
       // }
       //TODO group tags depending on open and close
-
-      if (tags.length > 0) xmlFields.push(tags);
+      let groups = [];
+      let jsonGroups = {}; // JSON.parse(JSON.stringify([]));
+      let currentGroupName = undefined;
+      // let groupOpenFlag = false;
+      for (const t of tags) {
+        const jsnT = JSON.parse(JSON.stringify(singleTag2Json(t)));
+        if (t.disposition === "Open") {
+          // create new group with the current element as the first element
+          // groupOpenFlag = true;
+          groups.push([t]);
+          currentGroupName = jsnT.name;
+          jsonGroups[currentGroupName] = [];
+          jsonForm[currentGroupName] = [{}];
+          // create the json tag for this one:
+        } else if (t.disposition === "Close") {
+          // set this element in the current group
+          // groupOpenFlag = false;
+          groups[groups.length - 1].push(t);
+          // set a new empty group
+          groups.push([]);
+          currentGroupName = undefined;
+        } else {
+          // disposition === 'SelfClosed'
+          if (currentGroupName) {
+            // we are adding elements in a group
+            groups[groups.length - 1].push(t);
+            jsonGroups[currentGroupName].push(jsnT);
+            //groups can have repetitions, but the initial json has only one repetition
+            // console.log("jsonForm: ", jsonForm);
+            jsonForm[currentGroupName][0][jsnT.name] = jsnT.rawText;
+          } else {
+            // the element is self contained
+            groups.push([t]);
+            jsonGroups[jsnT.name] = jsnT;
+            jsonForm[jsnT.name] = jsnT.rawText;
+          }
+        }
+      }
+      // console.log("jsonForm: ", jsonForm);
+      // console.log("Found groups: ", groups);
+      // now filter out empty groups and
+      const res = groups.filter((g) => g.length > 0);
+      // console.log("Filtered groups: ", res);
+      if (groups.length > 0) xmlFields.push(res);
+      jsonFields.push(jsonGroups);
+      // jsonForm.push(jsonForm);
+      // if (tags.length > 0) xmlFields.push(tags);
     }
     // export the result
-    return xmlFields;
+    console.log("FORM generated: ", jsonForm);
+
+    return {
+      obj: xmlFields,
+      json: jsonFields,
+      // form: JSON.parse(JSON.stringify(jsonForm)),
+      form: jsonForm,
+    };
   }
 }
 
@@ -76,15 +140,18 @@ async function extractTags(fname) {
   // return (templateFile, fields)
 }
 
-/**
- * Converts a list of tags into a json
- * @param {*} tagList A list of lists As returned by extractTags in this file
- */
-function tags2json(tagList) {
-  // TODO
-  // each list tagList can contain different groups of tags, I'll start by the simplest ones
-  for (const tag of tagList) {
+function jsnTags2jsnForm(tags) {
+  const jsnTags = JSON.parse(JSON.stringify(tags));
+  var jsnForm = {};
+
+  for (const group of jsnTags) {
+    // each is a part, header, body, footer or others
+    for (const tag of group) {
+    }
+    //
   }
+
+  return jsnForm;
 }
 
 async function fillTemplate(handler, template, data) {
